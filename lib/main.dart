1a441e0dca93d0b458b3e9339b0eca279d7dd0a5
Kento Yamazaki <chihayayome@yahoo.co.jp>
2020-12-26 21:46:53 +0900

    Implemented DB client on db_test.dart. main.dart still has a bug code. insert method does not return a value.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,109 @@
 import 'package:flutter/material.dart';
 
-void main() {
+import 'dart:async';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:flutter/widgets.dart';
+
+void main() async {
+
+  // Avoid errors caused by flutter upgrade.
+  // Importing 'package:flutter/widgets.dart' is required.
+  WidgetsFlutterBinding.ensureInitialized();
+  // Open the database and store the reference.
+  final Future<Database> database = openDatabase(
+    // Set the path to the database. Note: Using the `join` function from the
+    // `path` package is best practice to ensure the path is correctly
+    // constructed for each platform.
+      join(await getDatabasesPath(), 'doggie_database.db'),
+  // When the database is first created, create a table to store dogs.
+      onCreate: (db, version) {
+  return db.execute(
+  "CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)",
+  );
+  },
+  // Set the version. This executes the onCreate function and provides a
+  // path to perform database upgrades and downgrades.
+  version: 1,
+  );
+
+  Future<void> insertDog(Dog dog) async {
+    // Get a reference to the database
+    final Database db = await database;
+
+    await db.insert(
+        'dogs',
+        dog.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<Dog>> dogs() async {
+    // Get a reference to the database
+    final Database db = await database;
+
+    // Query the table for all the dogs
+    final List<Map<String, dynamic>> maps = await db.query('dogs');
+  }
+
+  Future<void> updateDog(Dog dog) async {
+    //Get a reference to the database
+    final db = await database;
+
+    //Update the given dog
+    await db.update(
+      'dogs',
+      dog.toMap(),
+      // Ensure that the dog has a matching id.
+      where: 'id = ?',
+      // Pass the Dog's id as a whereArg to prevent SQL injection
+      whereArgs: [dog.id],
+    );
+  }
+
+  Future<void> deleteDog(int id) async {
+    // Get a reference to the database.
+    final db = await database;
+
+    // Remove the dog from the database.
+    await db.delete(
+      'dogs',
+      //Use a 'where' clause to delete a specific dog
+      where: "id = ?",
+      // Pass the Dog's id as a whereArg to prevent SQL injection
+      whereArgs: [id],
+    );
+  }
+
+  var fido = Dog(
+    0,
+    'Fido',
+    35
+  );
+
+  print('iea');
+
+  //Insert a dog into the database.
+  await insertDog(fido);
+
+  //Print the list of dogs.
+  print(await dogs());
+
+  //Update Fido's age
+  fido = Dog(fido.id, fido.name, fido.age + 7);
+  await updateDog(fido);
+
+  //Print the list of dogs.
+  print(await dogs());
+
+  //Delete Fido
+  await deleteDog(fido.id);
+
+  //Print the list of dogs.
+  print(await dogs());
+
+  print('ieea');
 
 
   runApp(MyApp());
@@ -115,5 +218,30 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class Dog {
+  final int id;
+  final String name;
+  final int age;
+
+  Dog(this.id, this.name, this.age);
+
+  // Convert Dog into a Map. The keys must correspond to the names of
+  // columns in the database.
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+    };
+  }
+
+  // Implement toString to make it easier to see information about
+  // each dog when using the print statement.
+  @override
+  String toString() {
+    return 'Dog{id: $id, name: $name, age: $age}';
   }
 }
